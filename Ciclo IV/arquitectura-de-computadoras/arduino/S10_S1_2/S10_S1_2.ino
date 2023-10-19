df36298dab1ccb5cb3953fa1b03c4fc5f2b5bfcd
Luis Bazan <luis@bazan.pe>
2023-10-19 17:03:57 -0500

    Semana 10 arquitectura de computadoras

---
+++ Ciclo IV/arquitectura-de-computadoras/arduino/S10_S1_2/S10_S1_2.ino
@@ -0,0 +1,32 @@
+const int umbral;
+int LDR = A0;
+int LED = 3;
+
+int value;
+int i;
+
+void setup() {
+  pinMode(LDR, INPUT);
+  pinMode(LED, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  value = analogRead(LDR);
+
+  if(value > 900) {
+    digitalWrite(LED, 0);
+  } else {
+    digitalWrite(LED, 1);
+  }
+
+  graphValue(value-200, 7);
+  delay(50);
+}
+
+void graphValue(int value, int scale) {
+  for(i = 0; i < value / scale; i++) {
+    Serial.print("|");
+  }
+  Serial.println();
+}
