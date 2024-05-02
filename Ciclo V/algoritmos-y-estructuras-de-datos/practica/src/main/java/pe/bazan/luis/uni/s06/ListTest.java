0dfd4b420a8740df0c31d0b6498b0e9976f0442f
Luis Bazan <luis@bazan.pe>
2024-05-02 12:49:55 -0500

    Added CircularSinglyLinkedList

--- Ciclo V/algoritmos-y-estructuras-de-datos/practica/src/main/java/pe/bazan/luis/uni/s06/ListTest.java
+++ Ciclo V/algoritmos-y-estructuras-de-datos/practica/src/main/java/pe/bazan/luis/uni/s06/ListTest.java
@@ -7,31 +7,57 @@ public class ListTest {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		SinglyLinkedList<Company> list = new SinglyLinkedList<>();
+//		testSinglyLinkedList();
+		testCircularSinglyLinkedList();
+	}
 
-		list.add(0, new Company("!", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("A", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("B", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("C", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("D", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(2, new Company("A2", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(6, new Company("E", "Hi", 2023));
-		System.out.println(list.size());
+	private static void testCircularSinglyLinkedList() {
+		System.out.println("Test CircularSinglyLinkedList");
+		SinglyLinkedList<Company> singlyLinkedList = new SinglyLinkedList<>();
 
-		System.out.println(list);
-		System.out.println("--------");
-		System.out.println(list.get(6));
-		System.out.println(list.get(5));
-		System.out.println(list.get(4));
-		System.out.println(list.get(3));
-		System.out.println(list.get(2));
-		System.out.println(list.get(1));
-		System.out.println(list.get(0));
+		singlyLinkedList.add(0, new Company("!", "Hi", 2023));
+		singlyLinkedList.add(new Company("A", "Hi", 2023));
+		singlyLinkedList.add(new Company("B", "Hi", 2023));
+		singlyLinkedList.add(new Company("C", "Hi", 2023));
+		singlyLinkedList.add(new Company("D", "Hi", 2023));
+		singlyLinkedList.add(2, new Company("A2", "Hi", 2023));
+		singlyLinkedList.add(6, new Company("E", "Hi", 2023));
+
+		System.out.println(singlyLinkedList);
+		System.out.println("Reverse CircularSinglyLinkedList --------");
+		System.out.println(singlyLinkedList.get(6));
+		System.out.println(singlyLinkedList.get(5));
+		System.out.println(singlyLinkedList.get(4));
+		System.out.println(singlyLinkedList.get(3));
+		System.out.println(singlyLinkedList.get(2));
+		System.out.println(singlyLinkedList.get(1));
+		System.out.println(singlyLinkedList.get(0));
+		System.out.println("End test CircularSinglyLinkedList");
+		System.out.println();
+	}
+
+	private static void testSinglyLinkedList() {
+		System.out.println("Test SinglyLinkedList");
+		SinglyLinkedList<Company> singlyLinkedList = new SinglyLinkedList<>();
+
+		singlyLinkedList.add(0, new Company("!", "Hi", 2023));
+		singlyLinkedList.add(new Company("A", "Hi", 2023));
+		singlyLinkedList.add(new Company("B", "Hi", 2023));
+		singlyLinkedList.add(new Company("C", "Hi", 2023));
+		singlyLinkedList.add(new Company("D", "Hi", 2023));
+		singlyLinkedList.add(2, new Company("A2", "Hi", 2023));
+		singlyLinkedList.add(6, new Company("E", "Hi", 2023));
+
+		System.out.println(singlyLinkedList);
+		System.out.println("Reverse SinglyLinkedList --------");
+		System.out.println(singlyLinkedList.get(6));
+		System.out.println(singlyLinkedList.get(5));
+		System.out.println(singlyLinkedList.get(4));
+		System.out.println(singlyLinkedList.get(3));
+		System.out.println(singlyLinkedList.get(2));
+		System.out.println(singlyLinkedList.get(1));
+		System.out.println(singlyLinkedList.get(0));
+		System.out.println("End test SinglyLinkedList");
+		System.out.println();
 	}
 }
--- Ciclo V/algoritmos-y-estructuras-de-datos/practica/src/main/java/pe/bazan/luis/uni/s06/Main.java
+++ Ciclo V/algoritmos-y-estructuras-de-datos/practica/src/main/java/pe/bazan/luis/uni/s06/Main.java
@@ -1,36 +1,7 @@
 package pe.bazan.luis.uni.s06;
 
-import pe.bazan.luis.uni.s06.domain.Company;
-import pe.bazan.luis.uni.s06.domain.SinglyLinkedList;
-
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		SinglyLinkedList<Company> list = new SinglyLinkedList<>();
-
-		list.add(0, new Company("!", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("A", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("B", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("C", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(new Company("D", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(2, new Company("A2", "Hi", 2023));
-		System.out.println(list.size());
-		list.add(6, new Company("E", "Hi", 2023));
-		System.out.println(list.size());
-
-		System.out.println(list);
-		System.out.println("--------");
-		System.out.println(list.get(6));
-		System.out.println(list.get(5));
-		System.out.println(list.get(4));
-		System.out.println(list.get(3));
-		System.out.println(list.get(2));
-		System.out.println(list.get(1));
-		System.out.println(list.get(0));
 	}
 }
